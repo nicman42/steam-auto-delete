9ce2b1ff376cb74369bc0cc5777632397ca6ba84
Nico Zimmermann <6852640+nicman42@users.noreply.github.com>
2021-08-26 14:20:09 +0200

    python

---
+++ steam_auto_delete_conf.py
@@ -0,0 +1,7 @@
+steam_libraries = ['C:\\Program Files (x86)\\Steam', 'D:\steam']
+delete_days = 365
+whitelist = [
+    228980, # Steamworks Common Redistributables
+    250820, # SteamVR
+    719950, # Windows Mixed Reality for SteamVR
+    ]
